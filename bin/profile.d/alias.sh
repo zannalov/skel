35fff78c1feaba12fe9a3e40337d6b375b0d0c51
Jason Schmidt <Zannalov@GMail.com>
2013-02-23 14:54:36 -0500

    Aliases are generally a better way of doing this, just have to find a way of making certain ones only apply on certain machines

--- bin/profile.d/alias.sh
+++ bin/profile.d/alias.sh
@@ -3,7 +3,14 @@
 # If shell is interactive
 if ( echo $- | grep i >/dev/null 2>&1 ); then
 
-    # Remove all aliases
+    # First remove all aliases (no unexpected ones)
     unalias -a
+
+    alias df='df -H -T'
+    alias diff='diff --exclude=CVS --exclude=.svn --exclude=.git'
+    alias du='du --si --one-file-system --max-depth=1 --total'
+    alias free='free -l -m -t'
+    alias ls='ls --color=auto -v --si -F -i -l'
+    alias ps='ps -A --format=euser,egroup,tname,pid,ppid,rss,args --forest'
 
 fi
