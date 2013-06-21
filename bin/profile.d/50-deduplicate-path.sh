606ce620572891ed4f1af8caac86857b0b744e42
Jason Schmidt <Zannalov@GMail.com>
2013-06-21 11:02:00 -0400

    A few updates from in3983

--- bin/profile.d/50-deduplicate-path.sh
+++ bin/profile.d/50-deduplicate-path.sh
@@ -5,7 +5,7 @@ export PATH="$( echo "$PATH" | sed 's/^:\+//;s/:\+$//;s/:\+/:/;' )"
 
 # Remove all subsequent duplicate entries from the path, leaving only the first
 # of each entry in the intended order
-export PATH="$( echo "$PATH" | awk 'BEGIN{ORS=":";RS="[:\n]"}$0&&!a[$0]++' )"
+export PATH="$( echo "$PATH" | awk 'BEGIN{ORS=":";RS=":"}$0&&!a[$0]++' )"
 
 # Remove the trailing record separator character left behind by awk
 export PATH="${PATH%:}"
