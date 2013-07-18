cd1b55cbcd45df51e136a66645f17a4f80df36c1
Jason Schmidt <Zannalov@GMail.com>
2013-07-18 16:11:25 -0400

    Contextual alias file

--- bin/profile.d/alias.sh
+++ bin/profile.d/alias.sh
@@ -6,11 +6,21 @@ if ( echo $- | grep i >/dev/null 2>&1 ); then
     # First remove all aliases (no unexpected ones)
     unalias -a
 
-    alias df='df -H -T'
-    alias diff='diff --exclude=CVS --exclude=.svn --exclude=.git'
-    alias du='du --si --one-file-system --max-depth=1 --total'
-    alias free='free -l -m -t'
-    alias ls='ls --color=auto -v --si -F -i -l'
-    alias ps='ps -A --format=euser,egroup,tname,pid,ppid,rss,args --forest'
+    if [ "Darwin" = "$( uname )" ]; then
+        alias df='df -H'
+        alias diff='diff --exclude=CVS --exclude=.svn --exclude=.git'
+        alias du='du -x -d 1 -c'
+        alias free='free -l -m -t'
+        alias ls='ls -v -F -i -l'
+        alias ps='ps -A -o euser,egroup,tname,pid,ppid,rss,args'
+    fi
+    if [ "Linux" = "$( uname )" ]; then
+        alias df='df -H -T'
+        alias diff='diff --exclude=CVS --exclude=.svn --exclude=.git'
+        alias du='du --si --one-file-system --max-depth=1 --total'
+        alias free='free -l -m -t'
+        alias ls='ls --color=auto -v --si -F -i -l'
+        alias ps='ps -A --format=euser,egroup,tname,pid,ppid,rss,args --forest'
+    fi
 
 fi
