59575fecdde861999f45b9d41c6128b6673f14c3
Jason Schmidt <Zannalov@GMail.com>
2013-11-13 20:48:25 -0500

    When run in a root account, the extra newline at the end breaks things

--- bin/profile.d/50-deduplicate-path.sh
+++ bin/profile.d/50-deduplicate-path.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # First remove any leading, trailing, or multiple colons
-export PATH="$( echo "$PATH" | sed 's/^:\+//;s/:\+$//;s/:\+/:/;' )"
+export PATH="$( printf "$PATH" | sed 's/^:\+//;s/:\+$//;s/:\+/:/;' )"
 
 # Remove all subsequent duplicate entries from the path, leaving only the first
 # of each entry in the intended order
-export PATH="$( echo "$PATH" | awk 'BEGIN{ORS=":";RS=":"}$0&&!a[$0]++' )"
+export PATH="$( printf "$PATH" | awk 'BEGIN{ORS=":";RS=":"}$0&&!a[$0]++' )"
 
 # Remove the trailing record separator character left behind by awk
 export PATH="${PATH%:}"
