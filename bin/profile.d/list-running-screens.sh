0fe3209ad49d1e38cbbadc675bfa6ef029c90f4f
Jason Schmidt <Zannalov@GMail.com>
2015-12-10 00:20:33 -0500

    Have been trying out tmux for a while now, and I think I may switch

--- bin/profile.d/list-running-screens.sh
+++ bin/profile.d/list-running-screens.sh
@@ -4,7 +4,12 @@
 if ( echo $- | grep i >/dev/null 2>&1 ); then
 
     # If not in screen, list open screen sessions
-    if [[ "" == "${WINDOW}" ]]; then
+    if [[ "" == "${WINDOW}" ]] && [[ "" == "${TMUX_PANE}" ]]; then
+        echo "tmux list-sessions"
+        tmux list-sessions
+        echo
+
+        echo "screen -ls"
         screen -ls
     fi
 
