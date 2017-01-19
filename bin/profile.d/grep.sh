3dcc941eee7c4df7c957f6b79d6a5e67632b9b7a
Benjamin Dean <ben@benjamindean.com>
2017-01-18 20:06:24 -0800

    Update grep.sh
    Modifying to remove the references to the now deprecated `GREP_OPTIONS` which throws a deprecation warning.

--- bin/profile.d/grep.sh
+++ bin/profile.d/grep.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-export GREP_OPTIONS=""
-export GREP_OPTIONS="$GREP_OPTIONS"' --colour=auto' # I like color
+my_grep_options=(--color=auto) # I like color, without deprecation warnings
 
 if ( _version_gte "$( grep -V | head -n 1 | sed 's/[^0-9]*\([0-9]*\.[0-9]*\).*/\1/' )" "2.5.2" ); then
-    export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir="CVS"' # Exclude CVS directories
-    export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir=".svn"' # Exclude Subversion directories
-    export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir=".git"' # Exclude Git directory
+    # The following adds new options for grep to exclude: CVS, .cvs, .git, .hg, .svn directories
+    my_grep_options=(${my_grep_options[@]}  --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=CVS)
+    alias grep="${BASH_ALIASES[grep]:-grep} my_grep_options"
 fi
