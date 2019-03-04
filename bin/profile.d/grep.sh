2d4d92d679b10c77218bf78946e5228b69f96941
Jason Schmidt <zannalov@gmail.com>
2019-03-04 14:05:09 -0500

    Preferences are compatible with slightly earlier version

--- bin/profile.d/grep.sh
+++ bin/profile.d/grep.sh
@@ -3,7 +3,7 @@
 export GREP_OPTIONS=""
 export GREP_OPTIONS="$GREP_OPTIONS"' --colour=auto' # I like color
 
-if ( _version_gte "$( grep -V | head -n 1 | sed 's/[^0-9]*\([0-9]*\.[0-9]*\).*/\1/' )" "2.5.2" ); then
+if ( _version_gte "$( grep -V | head -n 1 | sed 's/[^0-9]*\([0-9\.]*\).*/\1/' )" "2.5.1" ); then
     export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir="CVS"' # Exclude CVS directories
     export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir=".svn"' # Exclude Subversion directories
     export GREP_OPTIONS="$GREP_OPTIONS"' --exclude-dir=".git"' # Exclude Git directory
